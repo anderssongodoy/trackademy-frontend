b834c5814706149cb674590a99730586c1b45053
anderssongodoy <anderssongodoygarcia@gmail.com>
2026-03-07 18:00:50 -0500

    fix: use common authority for universal Microsoft account login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment: AppEnvironment = {
   authSessionPath: '/api/v1/auth/session',
   authMicrosoftExchangePath: '/api/v1/auth/microsoft',
   defaultUniversityId: 1,
-  azureTenantId: 'organizations',
+  azureTenantId: 'common',
   azureFrontendClientId: '3036277a-1052-4e6a-a0d9-64fec65b3f5e'
 };
